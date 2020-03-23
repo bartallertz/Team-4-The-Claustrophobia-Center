533024c95c29218a9f80017ed08a51bb642dd4de
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2020-03-23 16:13:27 +0100

    Rustkamer v1
    Dit is de eerste versie van de rustkamer. met een basis versie van de teleport functie, een schilderij

---
+++ FirstVRgame/Assets/MyData/Scripts/RotateCube.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateCube : MonoBehaviour
+{
+    // public float spinForce;
+    // Start is called before the first frame update
+    public Image imgGaze;
+
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Rotate(0, spinForce * Time.deltaTime, 0);
+        if (gvrStatus) {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+
+        if(Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+            if (imgGaze.fillAmount == 1 && _hit.transform.CompareTag("Teleport"))
+            {
+                _hit.transform.gameObject.GetComponent<Teleport>().TeleportPlayer();
+            }
+        }
+    }
+    
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+
+    // public void ChangeSpin()
+    // {
+    //     spinForce = -spinForce;
+
+    // }
+}
---
+++ FirstVRgame/Assets/MyData/Scripts/TeleportLevels.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportLevels : MonoBehaviour
+{
+   public GameObject player;
+   public void TeleportPlayer()
+   {
+    //    player.transform.position = new Vector3(transform.position.x, transform.position.2f, transform.position.z +14f);
+   }
+}
